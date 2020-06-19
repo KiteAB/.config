110cc85451d7c2f0844973cbd13cb4bf5a9124a9
KiteAB <kiteabpl@outlook.com>
2020-06-19 21:37:07 +0800

    vi qwerty>colemak & packages updated

--- zsh/vi.zsh
+++ zsh/vi.zsh
@@ -1,14 +1,14 @@
 bindkey '^v' edit-command-line
 bindkey -v
-bindkey -M vicmd "n" vi-insert
-bindkey -M vicmd "N" vi-insert-bol
-bindkey -M vicmd "j" vi-backward-char
-bindkey -M vicmd "l" vi-forward-char
-bindkey -M vicmd "J" vi-beginning-of-line
-bindkey -M vicmd "L" vi-end-of-line
-bindkey -M vicmd "k" down-line-or-history
-bindkey -M vicmd "i" up-line-or-history
-bindkey -M vicmd "u" undo
+bindkey -M vicmd "k" vi-insert
+bindkey -M vicmd "K" vi-insert-bol
+bindkey -M vicmd "n" vi-backward-char
+bindkey -M vicmd "i" vi-forward-char
+bindkey -M vicmd "N" vi-beginning-of-line
+bindkey -M vicmd "I" vi-end-of-line
+bindkey -M vicmd "e" down-line-or-history
+bindkey -M vicmd "u" up-line-or-history
+bindkey -M vicmd "l" undo
 #bindkey -M vicmd "-" vi-rev-repeat-search
 bindkey -M vicmd "=" vi-repeat-search
 bindkey -M vicmd "h" vi-forward-word-end
